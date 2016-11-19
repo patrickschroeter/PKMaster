148bec4353fe3538ba6b025b74182a1fd87cab06
Patrick Schroeter <patrick.schroeter@hs-augsburg.de>
2016-11-19 18:41:58 +0100

    [UPDATE] applications route

--- src/app/main/applications/applications.component.ts
+++ src/app/main/applications/applications.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Application, State } from './../../swagger';
+
 @Component({
     selector: 'pk-applications',
     templateUrl: './applications.component.html',
@@ -7,180 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationsComponent implements OnInit {
 
-    private form: Array<Object>;
+    private applications: Application[];
 
     constructor() { }
 
     ngOnInit() {
-        this.form = [
+        this.applications = [
             {
-                elementType: 'datalist',
-                name: 'datalist',
-                label: 'Datalist',
-                required: true
+                id: 1,
+                state: State.NameEnum.available,
+                created: 20160731,
+                form: {
+                    title: 'Antrag auf Masterarbeit'
+                }
             },
             {
-                elementType: 'input',
-                name: 'email',
-                type: 'input',
-                required: true,
-                placeholder: 'E-Mail',
-                validations: [
-                    'useExternalEmail',
-                    'isEmail'
-                ],
-                styles: [
-                    'small'
-                ]
-            },
-            {
-                elementType: 'input',
-                name: 'password',
-                type: 'password',
-                required: true,
-                placeholder: 'Password',
-                validations: [
-                    'minLength',
-                    'maxLength'
-                ],
-                styles: [
-                    'small'
-                ]
-            },
-            {
-                elementType: 'input',
-                name: 'date',
-                type: 'date'
-            },
-            {
-                elementType: 'input',
-                name: 'text',
-            },
-            {
-                elementType: 'checkbox',
-                name: 'privacy',
-                label: 'Sell your Soul?',
-                validations: [
-                    'toBeTrue',
-                ]
-            },
-            {
-                elementType: 'select',
-                name: 'country',
-                label: 'Land',
-                required: true,
-                multiple: false,
-                options: [
-                    {
-                        value: 'de',
-                        label: 'Deutschland'
-                    },
-                    {
-                        value: 'fr',
-                        label: 'Frankreich'
-                    },
-                    {
-                        value: 'es',
-                        label: 'Spanien'
-                    },
-                    {
-                        value: 'it',
-                        label: 'Italien'
-                    },
-                    {
-                        value: 'ir',
-                        label: 'Irland'
-                    },
-                    {
-                        value: 'bel',
-                        label: 'Belgien'
-                    },
-                    {
-                        value: 'cr',
-                        label: 'Croatien'
-                    },
-                    {
-                        value: 'en',
-                        label: 'USA'
-                    }
-                ]
-            },
-            {
-                elementType: 'select',
-                name: 'country',
-                label: 'Land',
-                required: true,
-                multiple: true,
-                placeholder: 'Wo kommst du her?',
-                options: [
-                    {
-                        value: 'de',
-                        label: 'Deutschland'
-                    },
-                    {
-                        value: 'fr',
-                        label: 'Frankreich'
-                    },
-                    {
-                        value: 'es',
-                        label: 'Spanien'
-                    },
-                    {
-                        value: 'it',
-                        label: 'Italien'
-                    },
-                    {
-                        value: 'ir',
-                        label: 'Irland'
-                    },
-                    {
-                        value: 'bel',
-                        label: 'Belgien'
-                    },
-                    {
-                        value: 'cr',
-                        label: 'Croatien'
-                    },
-                    {
-                        value: 'en',
-                        label: 'USA'
-                    }
-                ]
-            },
-            {
-                elementType: 'radio',
-                name: 'language',
-                label: 'Sprache',
-                required: true,
-                options: [
-                    {
-                        value: 'de',
-                        label: 'deutsch'
-                    },
-                    {
-                        value: 'en',
-                        label: 'englisch'
-                    }
-                ]
-            },
-            {
-                elementType: 'textarea',
-                name: 'desc',
-                label: 'Description',
-                required: true,
-                value: 'de'
-            },
-            {
-                elementType: 'textarea',
-                name: 'info',
-                label: 'Info',
-                required: true,
-                placeholder: 'Something',
-                styles: [
-                    'small'
-                ]
+                id: 2,
+                state: State.NameEnum.available,
+                created: 20160731,
+                form: {
+                    title: 'Antrag auf Notenanrechnung'
+                }
             }
-        ];
+        ]
     }
 
     save(event) {
