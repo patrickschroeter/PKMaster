5cf7c7556bde46bb0a2a7c104ec7ee1ce388ad44
Patrick Schroeter <patrick.schroeter@hs-augsburg.de>
2016-11-27 16:43:16 +0100

    [UPDATE] floating

--- src/app/shared/elements/button/button.component.ts
+++ src/app/shared/elements/button/button.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, Output, EventEmitter, HostBinding } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostBinding, AfterViewInit, ElementRef, Renderer } from '@angular/core';
 
 @Component({
     selector: 'pk-button',
     templateUrl: './button.component.html',
     styleUrls: ['./button.component.scss']
 })
-export class ButtonComponent implements OnInit {
+export class ButtonComponent implements OnInit, AfterViewInit {
 
     @HostBinding('class.element') element = true;
 
@@ -14,9 +14,26 @@ export class ButtonComponent implements OnInit {
     @Input() type: string = 'button';
     @Output() onClick = new EventEmitter<any>();
 
-    constructor() { }
+    private options: String[];
+
+    constructor(private renderer: Renderer, private elementRef: ElementRef) { }
 
     ngOnInit() {
+        this.options = [
+            'rounded',
+            'light',
+            'disabled'
+        ]
+    }
+
+    ngAfterViewInit() {
+        let nativeElement = this.elementRef.nativeElement;
+        for (let i = 0, length = this.options.length; i < length; i++) {
+            let option = this.options[i];
+            if (nativeElement.hasAttribute(option)) {
+                this.renderer.setElementClass(nativeElement, `element--${option}`, true);
+            }
+        }
     }
 
     emit() {
