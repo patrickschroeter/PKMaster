64672bd9ed900d088a71dfadf9c6d8f9c4c93783
Patrick Schroeter <patrick.schroeter@hs-augsburg.de>
2017-01-21 11:19:40 +0100

    [TRANSLATE] applications-edit component

--- src/app/main/applications/applications-edit/applications-edit.component.ts
+++ src/app/main/applications/applications-edit/applications-edit.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 
 import { ApplicationService } from './../../../core';
 import { AlertService } from './../../../modules/alert';
+import { TranslationService } from './../../../modules/translation';
 
 import { Application } from './../../../swagger';
 
@@ -23,7 +24,9 @@ export class ApplicationsEditComponent implements OnInit {
         private router: Router,
         private activatedRoute: ActivatedRoute,
         private applicationService: ApplicationService,
-        private alert: AlertService) { }
+        private alert: AlertService,
+        private translationService: TranslationService
+    ) { }
 
     ngOnInit() {
 
@@ -35,7 +38,7 @@ export class ApplicationsEditComponent implements OnInit {
                     return this.onError(params['id']);
                 } else if ( application.status && ['rescinded', 'created'].indexOf(application.status.name) === -1) {
                     this.router.navigate(['/applications']);
-                    this.alert.setErrorHint('no-application-edit', `It's not allowed to edit this application`, 2000);
+                    this.alert.setErrorHint('no-application-edit', this.translationService.translate('errorApplicationEditPermitted'), 2000);
                     return;
                 }
                 this.application = application;
@@ -48,7 +51,7 @@ export class ApplicationsEditComponent implements OnInit {
 
     private onError(id: string) {
         this.router.navigate(['/applications']);
-        this.alert.setErrorHint('no-application-found', `The is no application with the requested Id: ${id}`, 2000);
+        this.alert.setErrorHint('no-application-found', this.translationService.translate('errorNoApplicationWithId', [id]), 2000);
     }
 
     saveApplication(form) {
@@ -59,14 +62,14 @@ export class ApplicationsEditComponent implements OnInit {
 
     submitApplication(application: Application) {
         this.applicationService.submitApplication(application).subscribe(result => {
-            this.alert.setSuccessHint(`submitApplication${application.id}`, 'Application submitted');
+            this.alert.setSuccessHint(`submitApplication${application.id}`, this.translationService.translate('applicationSubmitted'));
             this.router.navigate([`/applications/`, result.id]);
         });
     }
 
     deactivateApplication(application: Application) {
         this.applicationService.deactivateApplication(application).subscribe(result => {
-            this.alert.setSuccessHint(`deactivateApplication${application.id}`, 'Application deactivated');
+            this.alert.setSuccessHint(`deactivateApplication${application.id}`, this.translationService.translate('applicationDeactivated'));
             this.router.navigate([`/applications/`, result.id]);
         });
     }
