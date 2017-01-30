6b12eff2ac89312f8161387a463a192a915c6e71
Patrick Schroeter <patrick.schroeter@hs-augsburg.de>
2017-01-30 19:55:08 +0100

    [UPDATE] permissions

--- src/app/core/services/api/UserApi.mock.ts
+++ src/app/core/services/api/UserApi.mock.ts
@@ -10,34 +10,125 @@ import { AppUser } from './../../../swagger';
 export class UserApiMock {
 
     static PERMISSION = {
-        READAPPLICATIONS: 'ReadApplications',
-        EDITAPPLICATIONS: 'EditApplications',
-        READCONFERENCES: 'ReadConferences',
-        EDITCONFERENCES: 'EditConferences',
-        READFORMS: 'ReadForms',
-        EDITFORMS: 'EditForms',
-        READPERMISSIONS: 'ReadPermissions',
-        EDITPERMISSIONS: 'EditPermissions'
-    };
+        Permission: {
+            Read: 'ReadPermissions',
+            Edit: 'EditPermissions'
+        },
+        Roles: {
+            Read: 'ReadRoles',
+            Edit: 'EditRoles',
+            Delete: 'DeleteRoles'
+        },
+        Users: {
+            Read: 'ReadUsers',
+            Edit: 'EditUsers',
+            EditPassword: 'EditUsersPassword'
+        },
+        Application: {
+            Create: 'CreateApplications',
+            Read: 'ReadApplications',
+            Edit: 'EditApplications',
+            Comment: 'CommentApplications',
+            Delete: 'DeleteApplications',
+            Accept: 'AcceptApplications'
+        },
+        Conference: {
+            Read: 'ReadConferences',
+            Edit: 'EditConferences'
+        },
+        Forms: {
+            Read: 'ReadForms',
+            Edit: 'EditForms'
+        }
+    }
 
     static PERMISSIONS = {
-        ALL: [
-            UserApiMock.PERMISSION.READAPPLICATIONS,
-            UserApiMock.PERMISSION.EDITAPPLICATIONS,
-            UserApiMock.PERMISSION.READCONFERENCES,
-            UserApiMock.PERMISSION.EDITCONFERENCES,
-            UserApiMock.PERMISSION.READFORMS,
-            UserApiMock.PERMISSION.EDITFORMS,
-            UserApiMock.PERMISSION.READPERMISSIONS,
-            UserApiMock.PERMISSION.EDITPERMISSIONS
+        All: [
+            /** Permissions */
+            UserApiMock.PERMISSION.Permission.Read,
+            UserApiMock.PERMISSION.Permission.Edit,
+            /** Roles */
+            UserApiMock.PERMISSION.Roles.Read,
+            UserApiMock.PERMISSION.Roles.Edit,
+            UserApiMock.PERMISSION.Roles.Delete,
+            /** Users */
+            UserApiMock.PERMISSION.Users.Read,
+            UserApiMock.PERMISSION.Users.Edit,
+            UserApiMock.PERMISSION.Users.EditPassword,
+            /** Applications */
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Application.Read,
+            UserApiMock.PERMISSION.Application.Edit,
+            UserApiMock.PERMISSION.Application.Delete,
+            UserApiMock.PERMISSION.Application.Comment,
+            UserApiMock.PERMISSION.Application.Accept,
+            /** Conference */
+            UserApiMock.PERMISSION.Conference.Read,
+            UserApiMock.PERMISSION.Conference.Edit,
+            /** Forms */
+            UserApiMock.PERMISSION.Forms.Read,
+            UserApiMock.PERMISSION.Forms.Edit,
         ],
-        PARTIAL: [
-            UserApiMock.PERMISSION.READAPPLICATIONS,
-            UserApiMock.PERMISSION.READPERMISSIONS
+        Admin: [
+            UserApiMock.PERMISSION.Permission.Read,
+            UserApiMock.PERMISSION.Permission.Edit,
+            UserApiMock.PERMISSION.Roles.Read,
+            UserApiMock.PERMISSION.Roles.Edit,
+            UserApiMock.PERMISSION.Roles.Delete,
+            UserApiMock.PERMISSION.Users.Read,
+            UserApiMock.PERMISSION.Users.Edit,
+            UserApiMock.PERMISSION.Users.EditPassword,
+        ],
+        Principal: [
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Application.Read,
+            UserApiMock.PERMISSION.Application.Edit,
+            UserApiMock.PERMISSION.Application.Delete,
+            UserApiMock.PERMISSION.Application.Comment,
+            UserApiMock.PERMISSION.Application.Accept,
+            UserApiMock.PERMISSION.Conference.Read,
+            UserApiMock.PERMISSION.Conference.Edit,
+            UserApiMock.PERMISSION.Forms.Read,
+            UserApiMock.PERMISSION.Forms.Edit,
+        ],
+        Member: [
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Application.Read,
+            UserApiMock.PERMISSION.Application.Comment,
+            UserApiMock.PERMISSION.Conference.Read,
+            UserApiMock.PERMISSION.Forms.Read,
+        ],
+        Docent: [
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Conference.Read,
+            UserApiMock.PERMISSION.Forms.Read,
+        ],
+        Student: [
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Forms.Read,
+        ],
+
+        Observer: [
+            UserApiMock.PERMISSION.Application.Read,
+        ],
+        Secreteriat: [
+            UserApiMock.PERMISSION.Application.Create,
+            UserApiMock.PERMISSION.Application.Read,
+            UserApiMock.PERMISSION.Application.Edit,
         ]
     };
 
-    static USERS: AppUser[] = [{ id: 'b904cc6e-b3a6-42a9-8880-3096be1b6c61', email: 'patrick.schroeter@hotmail.de', password: 'password', token: 'TOKEN', firstname: 'Patrick', lastname: 'Schroeter', matNr: 949225, permissions: UserApiMock.PERMISSIONS.ALL }, { id: 'ee632373-432e-40f0-9f33-8cc6b684e673', email: 'stephan.reichinger@gmail.de', password: 'password', firstname: 'Stephan', lastname: 'Reichinger', permissions: UserApiMock.PERMISSIONS.PARTIAL }];
+    static USERS: AppUser[] = [
+        { id: 'b904cc6e-b3a6-42a9-8880-3096be1b6c61', email: 'patrick.schroeter@hotmail.de', password: 'password', token: 'TOKEN', firstname: 'Patrick', lastname: 'Schroeter', matNr: 949225, permissions: UserApiMock.PERMISSIONS.All },
+        { id: 'ee632373-432e-40f0-9f33-8cc6b684e673', email: 'stephan.reichinger@gmail.de', password: 'password', firstname: 'Stephan', lastname: 'Reichinger', permissions: UserApiMock.PERMISSIONS.Student },
+        { id: '1', email: 'admin@pk.de', password: 'password', firstname: 'Admin', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Admin },
+        { id: '2', email: 'principal@pk.de', password: 'password', firstname: 'Principal', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Principal },
+        { id: '3', email: 'member@pk.de', password: 'password', firstname: 'Member', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Member },
+        { id: '4', email: 'docent@pk.de', password: 'password', firstname: 'Docent', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Docent },
+        { id: '5', email: 'student@pk.de', password: 'password', firstname: 'Student', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Student },
+        { id: '6', email: 'observer@pk.de', password: 'password', firstname: 'Observer', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Observer },
+        { id: '7', email: 'secreteriat@pk.de', password: 'password', firstname: 'Secreteriat', lastname: 'PK', permissions: UserApiMock.PERMISSIONS.Secreteriat },
+    ];
 
     static USER: AppUser = UserApiMock.USERS[1];
 
--- src/app/core/services/permission/permission.service.spec.ts
+++ src/app/core/services/permission/permission.service.spec.ts
@@ -70,7 +70,7 @@ describe('PermissionService', () => {
          */
 
         describe('(no permission input)', () => {
-            beforeEach(() => { service.permissions = API.PERMISSIONS.PARTIAL; });
+            beforeEach(() => { service.permissions = API.PERMISSIONS.Admin; });
             it('should return true if input is null', () => {
                 expect(service.hasPermission(null)).toBe(true);
             });
@@ -91,22 +91,22 @@ describe('PermissionService', () => {
         describe('(no user permission)', () => {
             it('should return false if permissions is null', () => {
                 service.permissions = undefined;
-                expect(service.hasPermission(API.PERMISSION.READAPPLICATIONS)).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS])).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS], true)).toBe(false);
+                expect(service.hasPermission(API.PERMISSION.Application.Read)).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Edit, API.PERMISSION.Forms.Read])).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Edit, API.PERMISSION.Forms.Read], true)).toBe(false);
             });
             it('should return false if permissions is undefined', () => {
                 service.permissions = null;
-                expect(service.hasPermission(API.PERMISSION.READPERMISSIONS)).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS])).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS], true)).toBe(false);
+                expect(service.hasPermission(API.PERMISSION.Permission.Read)).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Read, API.PERMISSION.Forms.Read])).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Read, API.PERMISSION.Forms.Read], true)).toBe(false);
 
             });
             it('should return false if permissions is empty array', () => {
                 service.permissions = [];
-                expect(service.hasPermission(API.PERMISSION.READPERMISSIONS)).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS])).toBe(false);
-                expect(service.hasPermission([API.PERMISSION.EDITPERMISSIONS, API.PERMISSION.READFORMS], true)).toBe(false);
+                expect(service.hasPermission(API.PERMISSION.Permission.Read)).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Read, API.PERMISSION.Forms.Read])).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Permission.Read, API.PERMISSION.Forms.Read], true)).toBe(false);
             });
         });
 
@@ -115,14 +115,14 @@ describe('PermissionService', () => {
          */
 
         it('should return true if the permission is in permissions', () => {
-            service.permissions = API.PERMISSIONS.PARTIAL;
-            expect(service.hasPermission(API.PERMISSION.READAPPLICATIONS)).toBe(true);
-            expect(service.hasPermission(API.PERMISSION.READPERMISSIONS)).toBe(true);
+            service.permissions = API.PERMISSIONS.Admin;
+            expect(service.hasPermission(API.PERMISSION.Roles.Read)).toBe(true);
+            expect(service.hasPermission(API.PERMISSION.Permission.Read)).toBe(true);
         });
 
         it('should return false if the permission is not in permissions', () => {
-            service.permissions = API.PERMISSIONS.PARTIAL;
-            expect(service.hasPermission(API.PERMISSION.READFORMS)).toBe(false);
+            service.permissions = API.PERMISSIONS.Admin;
+            expect(service.hasPermission(API.PERMISSION.Forms.Read)).toBe(false);
         });
 
         /**
@@ -132,41 +132,41 @@ describe('PermissionService', () => {
         describe('should return true if the permissions are in permissions', () => {
 
             beforeEach(() => {
-                service.permissions = API.PERMISSIONS.ALL;
+                service.permissions = API.PERMISSIONS.All;
             });
 
             it('(all elements in the same order)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.EDITAPPLICATIONS,
-                    API.PERMISSION.EDITFORMS,
-                    API.PERMISSION.EDITPERMISSIONS,
-                    API.PERMISSION.READAPPLICATIONS,
-                    API.PERMISSION.READFORMS,
-                    API.PERMISSION.READPERMISSIONS
+                    API.PERMISSION.Application.Edit,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Permission.Read,
+                    API.PERMISSION.Application.Read,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Permission.Read
                 ])).toBe(true);
             });
 
             it('(all elements in different order)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.READAPPLICATIONS,
-                    API.PERMISSION.EDITFORMS,
-                    API.PERMISSION.READFORMS,
-                    API.PERMISSION.EDITPERMISSIONS,
-                    API.PERMISSION.EDITAPPLICATIONS,
-                    API.PERMISSION.READPERMISSIONS
+                    API.PERMISSION.Application.Read,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Permission.Read,
+                    API.PERMISSION.Application.Edit,
+                    API.PERMISSION.Permission.Read
                 ])).toBe(true);
             });
 
             it('(some elements)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.EDITPERMISSIONS,
-                    API.PERMISSION.READFORMS
+                    API.PERMISSION.Permission.Read,
+                    API.PERMISSION.Forms.Read
                 ])).toBe(true);
             });
 
             it('(doubled elements)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.EDITAPPLICATIONS, API.PERMISSION.EDITAPPLICATIONS
+                    API.PERMISSION.Application.Edit, API.PERMISSION.Application.Edit
                 ])).toBe(true);
             });
         });
@@ -174,25 +174,25 @@ describe('PermissionService', () => {
         describe('should return false if the permissions are not in permissions', () => {
 
             beforeEach(() => {
-                service.permissions = API.PERMISSIONS.ALL;
+                service.permissions = API.PERMISSIONS.All;
             });
 
             it('(all elements plus extra)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.READAPPLICATIONS,
-                    API.PERMISSION.EDITFORMS,
-                    API.PERMISSION.READFORMS,
-                    API.PERMISSION.EDITPERMISSIONS,
-                    API.PERMISSION.EDITAPPLICATIONS,
-                    API.PERMISSION.READPERMISSIONS,
+                    API.PERMISSION.Application.Read,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Forms.Read,
+                    API.PERMISSION.Permission.Read,
+                    API.PERMISSION.Application.Edit,
+                    API.PERMISSION.Permission.Read,
                     'extra'
                 ])).toBe(false);
             });
 
             it('(some elements plus extra)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.EDITPERMISSIONS,
-                    API.PERMISSION.READFORMS,
+                    API.PERMISSION.Permission.Read,
+                    API.PERMISSION.Forms.Read,
                     'extra'
                 ])).toBe(false);
             });
@@ -205,7 +205,7 @@ describe('PermissionService', () => {
 
             it('(doubled elements)', () => {
                 expect(service.hasPermission([
-                    API.PERMISSION.EDITAPPLICATIONS, API.PERMISSION.EDITAPPLICATIONS,
+                    API.PERMISSION.Application.Edit, API.PERMISSION.Application.Edit,
                     'extra',
                     'extra'
                 ])).toBe(false);
@@ -219,24 +219,24 @@ describe('PermissionService', () => {
         describe('(or)', () => {
 
             beforeEach(() => {
-                service.permissions = API.PERMISSIONS.PARTIAL;
+                service.permissions = API.PERMISSIONS.Admin;
             });
 
             it('should return true if the input string is in the permissions', () => {
-                expect(service.hasPermission(API.PERMISSION.READAPPLICATIONS, true)).toBe(true);
+                expect(service.hasPermission(API.PERMISSION.Permission.Read, true)).toBe(true);
             });
 
             it('should return true if one of the input strings is in the permissions', () => {
-                expect(service.hasPermission([API.PERMISSION.READPERMISSIONS], true)).toBe(true);
-                expect(service.hasPermission([API.PERMISSION.EDITAPPLICATIONS, API.PERMISSION.READPERMISSIONS], true)).toBe(true);
+                expect(service.hasPermission([API.PERMISSION.Permission.Read], true)).toBe(true);
+                expect(service.hasPermission([API.PERMISSION.Roles.Edit, API.PERMISSION.Permission.Read], true)).toBe(true);
             });
 
             it('should return false if the input string is not in the permissions', () => {
-                expect(service.hasPermission(API.PERMISSION.EDITAPPLICATIONS, true)).toBe(false);
+                expect(service.hasPermission(API.PERMISSION.Application.Edit, true)).toBe(false);
             });
 
             it('should return false if none of the input strings is in the permissios', () => {
-                expect(service.hasPermission([API.PERMISSION.EDITAPPLICATIONS, API.PERMISSION.EDITFORMS], true)).toBe(false);
+                expect(service.hasPermission([API.PERMISSION.Application.Edit, API.PERMISSION.Forms.Read], true)).toBe(false);
             });
         });
     });
