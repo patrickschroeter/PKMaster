238b13dd5de9bedc83178826318b4161a7274dae
Patrick Schroeter <patrick.schroeter@hs-augsburg.de>
2017-02-05 00:52:28 +0100

    [UPDATE] conference hover effects

--- src/app/shared/elements/button-animation-wrapper/button-animation-wrapper.component.spec.ts
+++ src/app/shared/elements/button-animation-wrapper/button-animation-wrapper.component.spec.ts
@@ -1,28 +1,31 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ButtonAnimationWrapperComponent } from './button-animation-wrapper.component';
 
 describe('ButtonAnimationWrapperComponent', () => {
-  let component: ButtonAnimationWrapperComponent;
-  let fixture: ComponentFixture<ButtonAnimationWrapperComponent>;
+    let component: ButtonAnimationWrapperComponent;
+    let fixture: ComponentFixture<ButtonAnimationWrapperComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ButtonAnimationWrapperComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ButtonAnimationWrapperComponent
+            ],
+            schemas: [NO_ERRORS_SCHEMA]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ButtonAnimationWrapperComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ButtonAnimationWrapperComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
